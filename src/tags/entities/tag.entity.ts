c050eeda606deca913d5ad50f78c8a9661eac66f
AJkhan <atiajabbar39@gmail.com>
2024-09-10 13:42:17 +0500

    api's updated

--- src/tags/entities/tag.entity.ts
+++ src/tags/entities/tag.entity.ts
@@ -1,33 +1,16 @@
 
-// import { Schema } from 'mongoose';
-
-// export const TagSchema = new Schema({
-//   id: { type: Schema.Types.UUID, required: true, unique: true },
-//   name: { type: String, required: true },
-//   created_at: { type: Date, default: Date.now },
-//   updated_at: { type: Date, default: Date.now },
-// });
-
 
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { v4 as uuidv4 } from 'uuid';
+
 
 export type TagDocument = Tag & Document;
 
-@Schema()
+@Schema({timestamps: true, collection: "tl_tags"})
 export class Tag {
-  @Prop({ type: String, default: uuidv4, unique: true })
-  id: string;
 
   @Prop({ required: true })
   name: string;
-
-  @Prop({ default: Date.now })
-  created_at: Date;
-
-  @Prop({ default: Date.now })
-  updated_at: Date;
 }
 
 export const TagSchema = SchemaFactory.createForClass(Tag);
--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,6 +1,6 @@
 
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Tag } from '../tags/dto/tags.dto';
@@ -18,15 +18,33 @@ export class TagsService {
     return this.tagModel.find().exec();
   }
 
+ 
+
   async findOne(id: string): Promise<Tag> {
-    return this.tagModel.findById(id).exec();
+    const tag = await this.tagModel.findById(id).exec();
+    if (!tag) {
+      throw new NotFoundException(`Tag with ID "${id}" not found`);
+    }
+    return tag;
   }
+
+ 
 
   async update(id: string, updateTagDto: Partial<Tag>): Promise<Tag> {
-    return this.tagModel.findByIdAndUpdate(id, updateTagDto, { new: true }).exec();
+    const updatedTag = await this.tagModel.findByIdAndUpdate(id, updateTagDto, { new: true }).exec();
+    if (!updatedTag) {
+      throw new NotFoundException(`Tag with ID "${id}" not found`);
+    }
+    return updatedTag;
   }
 
+
+
   async delete(id: string): Promise<Tag> {
-    return this.tagModel.findByIdAndDelete(id).exec();
+    const deletedTag = await this.tagModel.findByIdAndDelete(id).exec();
+    if (!deletedTag) {
+      throw new NotFoundException(`Tag with ID "${id}" not found`);
+    }
+    return deletedTag;
   }
 }
